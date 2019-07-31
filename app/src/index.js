6658ab310e84ea5ab834a08468db2164537f886d
stevekutz <stkutz@gmail.com>
2019-07-30 19:48:09 -0500

    added in rootReducer

--- app/src/index.js
+++ app/src/index.js
@@ -4,7 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import rootReducer from './reducers';
+//const store = createStore(rootReducer);
+
+// Needed for Redux DevTool state debugging in Chrome!
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+
+ReactDOM.render(
+  <Provider store = {store}> 
+    <Router>
+        <App/>
+    </Router>
+  </Provider>, 
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
