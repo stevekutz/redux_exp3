9819dd07f16690be29fa31cab2ebe19545743b1f
stevekutz <stkutz@gmail.com>
2019-08-02 17:09:02 -0500

    added Episodes component

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ import {Route, NavLink} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <h1> Get ready to build some Routes!</h1>
+      <h1> Get ready to build some Routes!!!!</h1>
 
 
     </div>
---
+++ app/src/components/Episodes.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import {deleteWatched} from '../actions/actionsEpisodes';
+
+import Episode from './Episode';
+
+const Episodes = props => {
+
+    return (
+        <div>
+            {props.episodes.map( (episode, index) => {
+                <Episode
+                    key = {index}
+                    {...episode}
+                />
+            })}
+
+            <button                 // NOT props.deleteWatched
+               onClick = { () => {props.deleteWatched() } }
+            
+            >              
+                Delete Watched
+            </button>            
+        </div>
+
+
+    )
+}
+
+// orig
+const mapStateToProps = state => {
+    return {
+        episodes: state.episodes // as initialState in reducer
+    }
+};
+/*
+// cleaner
+const mapStateToProps = state => (
+    {episodes: state.episodes}
+);
+
+// sleek
+const mapStateToProps = state => ({episodes: state.episodes});
+*/
+
+export default connect(
+        mapStateToProps,
+        {deleteWatched} // action creator
+)(Episodes);
