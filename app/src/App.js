092dd7f048213105a29dcd3ab229a354644cd0a5
stevekutz <stkutz@gmail.com>
2019-08-02 21:58:34 -0500

    experimenting wtih background img stylings

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
+import tzImg from './img/twz_01.jpeg';
 
 import {Route, NavLink} from 'react-router-dom';
 
 import Episodes from './components/Episodes';
 import AddEpisode from './views/AddEpisode';
+// import { url } from 'inspector';
 
+// app/src/img/twz_01.jpeg
 
-
-/*
+// point to local file 
+// backgroundImage: `url(${tzImg})`,
+// point to link
+// backgroundImage: `url(https://i1.wp.com/www.thehomicidalhomemaker.com/wp-content/uploads/2018/05/twilight-zone-mystic-seer-candy-box-bw.png)`,
 function App() {
   return (
-    <div className="App">
+    <div className="App"
+      style={{
+        backgroundImage: `url(${tzImg})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '90%',
+        margin: "5px auto",
+        position: "absolute",
+        zIndex : "-1",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+    >
       <h1> Get ready to build some Routes!!!!</h1>
         <Route 
           path = '/'
@@ -41,8 +61,8 @@ function App() {
     </div>
   );
 }
-*/
 
+/*
 class App extends React.Component {
   render() {
     return (
@@ -81,5 +101,5 @@ class App extends React.Component {
 
 
 }
-
+*/
 export default App;
